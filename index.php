250ef12a8b952c6ec6f8bf2f59540ed0ae42c2c0
dogtreat268 <34668204+dogtreat268@users.noreply.github.com>
2021-01-13 11:16:58 -0500

    comment search

--- index.php
+++ index.php
@@ -13,13 +13,48 @@
     $sql = "SELECT * FROM sweetwater_test";
     $results = $conn->query($sql);
 
-    
+    //arrays to store the different types of comments
+    $candy_comments = array();
+    $call_comments = array();
+    $referred_comments = array();
+    $signature_comments = array();
+    $misc_comments = array();
+
     //make sure data was recieved
     if($results->num_rows > 0)
     {
         while($row = $results->fetch_assoc())
         {
-            
+            //convert the comment to lower case so caps don't mess up the search
+            $search_comment = strtolower($row["comments"]);
+
+            //check if the search comment contains one of our search strings if it does
+            //push it to the corresponding array, if none of the searchs find a match 
+            //put the comment in the miscellaneous array
+            if(str_contains($search_comment, "candy"))
+            {
+                array_push($candy_comments, $row["comments"]);
+            }
+            else if(str_contains($search_comment, "call"))
+            {
+                array_push($call_comments, $row["comments"]);
+            }
+            else if(str_contains($search_comment, "referred"))
+            {
+                array_push($referred_comments, $row["comments"]);
+            }
+            else if(str_contains($search_comment, "signature"))
+            {
+                array_push($signature_comments, $row["comments"]);
+            }
+            else if(str_contains($search_comment, "referred"))
+            {
+                array_push($referred_comments, $row["comments"]);
+            }
+            else
+            {
+                array_push($misc_comments, $row["comments"]);
+            }
         }
     }
 
@@ -34,6 +69,6 @@
     <title>Document</title>
 </head>
 <body>
-    Test
+
 </body>
 </html>
